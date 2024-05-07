38f1ba59f9267d1546f807220859cadc421333c3
Seungjin Kim <seungjin@duck.com>
2024-05-07 18:09:20 +0900

    with spin main branch, currently 3.x

--- examples/fetch-streaming/src/lib.rs
+++ examples/fetch-streaming/src/lib.rs
@@ -1,6 +1,9 @@
-use futures::SinkExt;
 use futures::stream::StreamExt;
-use spin_sdk::http::{Fields, IncomingRequest, OutgoingResponse, ResponseOutparam};
+use futures::SinkExt;
+// use spin_executor::bindings::wasi::io::streams;
+use spin_sdk::http::{
+    Fields, IncomingRequest, OutgoingResponse, ResponseOutparam,
+};
 use spin_sdk::http_component;
 
 #[derive(Debug, sqlx::FromRow)]
@@ -13,15 +16,21 @@ struct Pet {
 impl std::fmt::Display for Pet {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let fin_desc = if self.is_finicky { "is" } else { "is not" };
-        f.write_fmt(format_args!("{}, aged {}, {} finicky", self.name, self.age, fin_desc))
+        f.write_fmt(format_args!(
+            "{}, aged {}, {} finicky",
+            self.name, self.age, fin_desc
+        ))
     }
 }
 
 #[http_component]
 async fn handle_fetch_many(_req: IncomingRequest, resp: ResponseOutparam) {
-    let og = OutgoingResponse::new(200, &Fields::new(&[
-        ("content-type".into(), "text/plain".into())
-    ]));
+    let mut headers = spin_sdk::http::Headers::new();
+    headers.append(
+        &"content-type".to_string(),
+        &"text/plain".as_bytes().to_vec(),
+    );
+    let og = OutgoingResponse::new(headers);
     let mut resp_stm = og.take_body();
     resp.set(og);
 
@@ -39,7 +48,7 @@ async fn handle_fetch_many(_req: IncomingRequest, resp: ResponseOutparam) {
 
     let mut resp_lines = pets.map(|pet| match pet {
         Ok(pet) => Ok(format!("{pet}\n").into_bytes()),
-        Err(e) => Err(spin_sdk::http::Error::UnexpectedError(format!("{e:?}"))),
+        Err(e) => panic!(),
     });
 
     _ = resp_stm.send_all(&mut resp_lines).await;
